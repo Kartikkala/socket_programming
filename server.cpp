e9cf7d9fffef46ead7628ea2b670a7f3cda10c51
Kartikkala <kartikkala10december@gmail.com>
2024-12-31 19:55:10 +0530

    Corrected server implementation

--- server.cpp
+++ server.cpp
@@ -1,47 +1,71 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <string.h>
+#include <cstring>
 #include <unistd.h>
+#include <cstdlib>
+#include <cerrno>
 
+int main() {
+    const char socketPath[] = "/tmp/mysock";
+    char *buf = new char[40];
+    if (remove(socketPath) == -1 && errno != ENOENT) {
+        perror("remove");
+        delete[] buf;
+        return EXIT_FAILURE;
+    }
 
-int main()
-{
-    char *buf = (char*)malloc(sizeof(char) * 40);
     int sfd = socket(AF_UNIX, SOCK_STREAM, 0);
-    int cfd = 0;
-    int readStatus = 0;
-    char socketPath[] = "/tmp/mysock";
-    remove(socketPath);
-    printf("%d", remove(socketPath));
-    if(sfd == -1)
+    if (sfd == -1) {
         perror("socket");
-    struct sockaddr_un addr;
+        delete[] buf;
+        return EXIT_FAILURE;
+    }
 
+    struct sockaddr_un addr;
     memset(&addr, 0, sizeof(struct sockaddr_un));
     addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, socketPath, sizeof(addr.sun_path)-1);
+    strncpy(addr.sun_path, socketPath, sizeof(addr.sun_path) - 1);
 
-    if(bind(sfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1)
+    if (bind(sfd, (struct sockaddr*)&addr, sizeof(struct sockaddr_un)) == -1) {
         perror("bind");
+        close(sfd);
+        delete[] buf;
+        return EXIT_FAILURE;
+    }
 
-    if(listen(sfd, 20) == -1)
+    if (listen(sfd, 20) == -1) {
         perror("listen");
+        close(sfd);
+        delete[] buf;
+        return EXIT_FAILURE;
+    }
 
-    while(true)
-    {
-        cfd = accept(sfd, NULL, 0);
-        if(cfd == -1)
+    while (true) {
+        int cfd = accept(sfd, nullptr, nullptr);
+        if (cfd == -1) {
             perror("accept");
-        while((readStatus = read(cfd, (char *)buf, 40)) > 0)
-        {
-            if(write(STDOUT_FILENO, buf, 40) != readStatus)
-                perror("Partial read/write");
-            if(readStatus == -1)
-                perror("Read error from client socket!");
-            if(close(cfd) == -1)
-                perror("Close");
+            continue;
+        }
+
+        int readStatus;
+        while ((readStatus = read(cfd, buf, 40)) > 0) {
+            if (write(STDOUT_FILENO, buf, readStatus) != readStatus) {
+                perror("write");
+                break;
+            }
+        }
+
+        if (readStatus == -1) {
+            perror("read");
+        }
+
+        if (close(cfd) == -1) {
+            perror("close client socket");
         }
     }
-    exit(EXIT_SUCCESS);
+
+    close(sfd);
+    delete[] buf;
+    return EXIT_SUCCESS;
 }
